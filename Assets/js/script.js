0db854c2b895b8683f9e045bc57a51f8b899f4d1
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-02-28 16:43:20 -0500

    Updated readme file

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,14 +3,9 @@
 // in the html.
 $(document).ready(
   $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-
+    // Gets date in form of {Day name}, {Full month name} {day}
     let dayJSobject = dayjs().format("dddd, MMMM D");
+    // Gets the current hour of the day
     let hour = dayjs().hour();
 
     $(".saveBtn").on("click", function() {
@@ -23,13 +18,6 @@ $(document).ready(
       // Saves anything that we input in the text box to the parent ID of the save button
       localStorage.setItem(time, text);
     })
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-
 
     // Selects all the time blocks
     let timeBlocks = $(".px-5").children();
@@ -63,18 +51,8 @@ $(document).ready(
         }
       });
     }
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
 
-    // timeBlocks.each(function() {
-    //   // console.log("ID getting retrieved: " + this.id);
-    //   $(this .description).val(localStorage.getItem(this.id));
-    //   console.log($(this .description).val());
-    // });
-
+    // Retrieves items from Local Storage and displays it on the time blocks as necessary
     $("#hour-9 .description").val(localStorage.getItem("hour-9"));
     $("#hour-10 .description").val(localStorage.getItem("hour-10"));
     $("#hour-11 .description").val(localStorage.getItem("hour-11"));
@@ -85,7 +63,7 @@ $(document).ready(
     $("#hour-4 .description").val(localStorage.getItem("hour-4"));
     $("#hour-5 .description").val(localStorage.getItem("hour-5"));
 
-    // TODO: Add code to display the current date in the header of the page.
+    // Displays the current date in the header of the page
     $("#currentDay").text(dayJSobject + "th");
 
     timeTracker();
